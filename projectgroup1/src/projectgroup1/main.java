b9c499c371974245139669892bf7cfa39bd3bc00
Aristide <gnaristide@gmail.com>
2026-03-18 16:25:46 +0200

    new project

---
+++ projectgroup1/src/projectgroup1/main.java
@@ -0,0 +1,12 @@
+package projectgroup1;
+
+public class main {
+
+	public static void main(String[] args) {
+		String name = "NGA";
+		// TODO Auto-generated method stub
+		//Hello class, my name is ......
+		System.out.println("Hello class, my name is "+name+" and i thank you.");
+	}
+
+}
